82ab9b9e513848489b16cd852452cd24c525c3fb
Fida-VS <poletaeva69vera@mail.ru>
2024-07-12 13:03:11 +0300

    change browserrouter

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='https://github.com/Fida-VS/test-task-pryaniky'>
     <Provider store={store}>
     <App />
     </Provider>
