4b8c36739257b4af443e5eb16a867f745612c59d
Fida-VS <poletaeva69vera@mail.ru>
2024-07-12 14:36:56 +0300

    add in package-json

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='https://github.com/Fida-VS/test-task-pryaniky'>
+    <BrowserRouter>
     <Provider store={store}>
     <App />
     </Provider>
