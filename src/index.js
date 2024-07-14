fd35a64eacbddd5061a90e40ff1d91d8b624937c
Fida-VS <poletaeva69vera@mail.ru>
2024-07-14 15:14:09 +0300

    some changes

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import store from './store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename='/test-task-pryaniky/'>
     <Provider store={store}>
     <App />
     </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
